24936c6673734571d0c69a2f12ff5b8f9a335be1
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2022-04-27 12:21:07 -0400

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,9 +8,7 @@ const Login = (props) => {
           <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
           <SignUp>GET ALL THERE</SignUp>
           <Description>
-            Get Premier Access to Raya and the Last Dragon for an additional fee
-            with a Disney+ subscription. As of 03/26/21, the price of Disney+
-            and The Disney Bundle will increase by $1.
+            Stream now. Terms Apply.
           </Description>
           <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
         </CTA>
